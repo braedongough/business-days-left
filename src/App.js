e7722fdcf3ee346894fe72689e0551a9af1d2117
Braedon <braedongough@gmail.com>
2019-02-14 20:58:48 +0100

    ⚡️

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import moment from "moment";
+import "moment-business-days";
 import "./App.css";
+import Headline from "./components/Headline";
+
+//moment months are Zero indexed, don't forget.
+//*date format "yyyy-mm-dd"
+//!moment().businessDiff(moment().endOf("month"))
+//^number of business days remaining in the month
 
 class App extends Component {
+  state = {
+    month: moment()
+      .format("MMMM")
+      .toUpperCase(),
+    remainingBusinessDays: moment().businessDiff(moment().endOf("month")),
+  };
+  componentDidMount() {
+    console.log(moment().format("MMMM"));
+  }
+
   render() {
     return (
       <div className="App">
-        <p>butt</p>
+        <Headline month={this.state.month} />
+        <p className="huge">{this.state.remainingBusinessDays}</p>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/Headline.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Headline = props => (
+  <div>
+    <h1>HOW MANY BUSINESS DAYS ARE LEFT IN {props.month}?</h1>
+  </div>
+);
+
+export default Headline;
